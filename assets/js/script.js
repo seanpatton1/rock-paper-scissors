8725dec842d9284f41dcfc60bdc4d918cf5603db
seanpatton1 Patton <seanpatton1@outlook.com>
2024-04-20 14:12:15 +0000

    added constants for game and added notes to structure my approach

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let playerScore = 0;
+let computerScore = 0;
+
+// constants declared for DOM elements and user/computer chocies
+const userScoreSpan = document.getElementById('user-score');
+const computerScoreSpan = document.getElementById('computer-score');
+const gameMessage = document.getElementById('game-message').querySelector('h1');
+const rockBtn = document.querySelector('[data-choice="0"]');
+const paperBtn = document.querySelector('[data-choice="1"]');
+const scissorsBtn = document.querySelector('[data-choice="2"]');
+const resetBtn = document.querySelector('.reset-btn');
+const userImage = document.getElementById('user').querySelector('img');
+const computerImage = document.getElementById('computer').querySelector('img');
+const choices = ['rock', 'paper' 'scissors'];
+
+// Function to randomly select the computer's choice
+
+// Function to determine the winner of a round
+
+// Function called when the player makes a choice
+
+// Event listeners for the game buttons
+
+// Reset the scoresgit status
