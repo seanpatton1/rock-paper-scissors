20caab1f1d0989a22497375fa04eb28ec88791c1
seanpatton1 Patton <seanpatton1@outlook.com>
2024-04-20 15:09:12 +0000

    added function for player choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,43 @@ const scissorsBtn = document.querySelector('[data-choice="2"]');
 const resetBtn = document.querySelector('.reset-btn');
 const userImage = document.getElementById('user').querySelector('img');
 const computerImage = document.getElementById('computer').querySelector('img');
-const choices = ['rock', 'paper' 'scissors'];
 
 // Function to randomly select the computer's choice
+function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+}
 
 // Function to determine the winner of a round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ("it's a tie");
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+        (playerSelection === 'paper' && computerSelection === 'rock')
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        return "You win!" + playerSelection + "beats" + computerSelection;
+    } else {
+        computerScore++;
+        return "You lost!" + computerSelection + "beats" + playerSelection;
+    }
+}
+
 
 // Function called when the player makes a choice
+function playerChoice(playerSelection) {
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    // Display the result
+    messageDiv.textContent = result;
+    userScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
+    userImage.src = 'assets/images/' + playerSelection + '.webp';
+    computerImage.src = 'assets/images/' + computerSelection + '.webp';
+}
 
 // Event listeners for the game buttons
 
